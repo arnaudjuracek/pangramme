f692afd3989c8052cf498c0c91854e9ea42df2c7
Arnaud Juracek <arnaud.juracek+github@gmail.com>
2015-06-18 16:36:33 +0200

    Add 401 error handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,26 +7,19 @@ $(document).ready(function(){
 });
 
 var load_font = function(url){
-	$('header .title').html(url.substring(url.lastIndexOf('/')+1) || 'Arial');
-	$('head > style#fontLoader').html("\
+	url = 'https://' + url;
+	$.post(url,function(){
+		$('header .title').html(url.substring(url.lastIndexOf('/')+1) || 'Arial (default)');
+		$('head > style#fontLoader').html("\
 			@font-face {\
 				font-family: 'fromURL';\
-				src: url('https://"+url+"') format('truetype');\
+				src: url('"+url+"') format('truetype');\
 				font-weight: normal;\
 				font-style: normal;\
 				font-smooth: never;\
 				-webkit-font-smoothing : none;\
 			}");
-
-	// $('head').append("\
-	// 	<style type='text/css'>\
-	// 		@font-face {\
-	// 			font-family: 'fromURL';\
-	// 			src: url('"+url+"') format('truetype');\
-	// 			font-weight: normal;\
-	// 			font-style: normal;\
-	// 			font-smooth: never;\
-	// 			-webkit-font-smoothing : none;\
-	// 		}\
-	// 	</style>");
+	}).error(function(){
+		console.log("401 : font can't be loaded");
+	});
 }
